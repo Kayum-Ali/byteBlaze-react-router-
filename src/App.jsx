8b50f2bf428c568c022087607cf5bccbdaefdc77
Kayum Ali <sisirfahimstudent173@gmail.com>
2024-03-22 06:34:11 +0600

    project setup done and navbar added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+import Home from './pages/Home'
+
+function App() {
+
+
+  return (
+    <>
+      <Home></Home>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Nav from "../Components/Nav";
+
+
+const Home = () => {
+    return (
+        <div>
+            {/* NavBar */}
+            <Nav></Nav>
+            <h1>HomePage</h1>
+
+        </div>
+    );
+};
+
+export default Home;
